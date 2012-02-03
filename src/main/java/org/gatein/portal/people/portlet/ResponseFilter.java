79648b1afa018684f0bbad9bbda01ab3a2709069
Julien Viet <julien@julienviet.com>
2012-02-03 17:16:24 +0100

    select user when clicking on it

--- src/main/java/org/gatein/portal/people/portlet/ResponseFilter.java
+++ src/main/java/org/gatein/portal/people/portlet/ResponseFilter.java
@@ -21,6 +21,12 @@ public class ResponseFilter implements RenderFilter
 
    public void doFilter(RenderRequest request, RenderResponse response, FilterChain chain) throws IOException, PortletException
    {
+      Element css1 = response.createElement("link");
+      css1.setAttribute("rel", "stylesheet/less");
+      css1.setAttribute("href", request.getContextPath() + "/css/main.less");
+      response.addProperty(MimeResponse.MARKUP_HEAD_ELEMENT, css1);
+
+      //
       Element jQuery1 = response.createElement("script");
       jQuery1.setAttribute("type", "text/javascript");
       jQuery1.setAttribute("src", request.getContextPath() + "/js/jquery-1.7.1.min.js");
@@ -31,12 +37,6 @@ public class ResponseFilter implements RenderFilter
       jQuery2.setAttribute("type", "text/javascript");
       jQuery2.setAttribute("src", request.getContextPath() + "/js/less-1.2.1.min.js");
       response.addProperty(MimeResponse.MARKUP_HEAD_ELEMENT, jQuery2);
-
-      //
-      Element css1 = response.createElement("link");
-      css1.setAttribute("rel", "stylesheet/less");
-      css1.setAttribute("href", request.getContextPath() + "/css/main.less");
-      response.addProperty(MimeResponse.MARKUP_HEAD_ELEMENT, css1);
 
       //
       chain.doFilter(request, response);
