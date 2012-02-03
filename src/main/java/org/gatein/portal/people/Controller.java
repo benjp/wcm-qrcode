49e42d93bc64ab523890893c7a2927fa6e33c68e
Julien Viet <julien@julienviet.com>
2012-02-03 16:07:57 +0100

    add users in the users template

--- src/main/java/org/gatein/portal/people/Controller.java
+++ src/main/java/org/gatein/portal/people/Controller.java
@@ -1,12 +1,16 @@
 package org.gatein.portal.people;
 
+import org.exoplatform.commons.utils.ListAccess;
 import org.exoplatform.services.organization.OrganizationService;
+import org.exoplatform.services.organization.User;
+import org.exoplatform.services.organization.UserHandler;
 import org.juzu.Resource;
 import org.juzu.View;
 import org.juzu.Path;
 import org.juzu.template.Template;
 import javax.inject.Inject;
 import java.io.IOException;
+import java.util.Collections;
 
 /** @author <a href="mailto:julien.viet@exoplatform.com">Julien Viet</a> */
 public class Controller
@@ -25,10 +29,14 @@ public class Controller
    /** . */
    private final OrganizationService orgService;
 
+   /** . */
+   private final UserHandler userHandler;
+
    @Inject
    public Controller(OrganizationService orgService)
    {
       this.orgService = orgService;
+      this.userHandler = orgService.getUserHandler();
    }
 
    @View
@@ -38,8 +46,12 @@ public class Controller
    }
    
    @Resource
-   public void users() throws IOException
+   public void users() throws Exception
    {
-      users.render();
+      int from = 0;
+      int size = 10;
+      ListAccess<User> userList = userHandler.findAllUsers();
+      int length = Math.min(from + size, userList.getSize());
+      users.render(Collections.singletonMap("users", userList.load(from, length)));
    }
 }
