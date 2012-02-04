775124d0761f946f61c1a979315e1bfa0188c803
Julien Viet <julien@julienviet.com>
2012-02-04 22:31:50 +0100

    replace checkbox with push pin icon

--- src/main/webapp/js/people.js
+++ src/main/webapp/js/people.js
@@ -21,30 +21,37 @@ $(function () {
   });
 
   //
-  $("#people").on("click", ".user", function () {
-    if ($("#users input[type='checkbox']:checked").size() == 0)
-    {
-      $("#users .user.selected input[type='checkbox']:not(:checked)").parents(".user").removeClass("selected");
-      $(this).addClass("selected");
-      $("#groups-search input[type=hidden]").remove();
-      $("<input name='userName' type='hidden'/>").appendTo("#groups-search").val($(this).attr("id"));
-      searchGroups();
-    }
-  });
-  $("#people").on("click", ".user input[type='checkbox']", function(e) {
-    if ($(this).is (":checked")) {
-      $("#users .user.selected input[type='checkbox']:not(:checked)").parents(".user").removeClass("selected").each(function() {
-        $("#groups-search input[type=hidden][value=" + $(this).attr("id") + "]").remove();
-      });
-      $(this).parents(".user").addClass("selected");
-      $("<input name='userName' type='hidden'/>").appendTo("#groups-search").val($(this).parents(".user").attr("id"));
-    } else {
-      var foo = $(this).parents(".user");
-      $("#groups-search input[type=hidden][value=" + foo.attr("id") + "]").remove();
-      foo.removeClass("selected");
-    }
+  var addUser = function() {
+    $("<input name='userName' type='hidden'/>").appendTo("#groups-search").val($(this).attr("id"));
+  };
+  var removeUser = function() {
+    $("#groups-search input[type=hidden][value=" + $(this).attr("id") + "]").remove();
+  };
+
+  $("#users").on("click", ".checkable:not(.checked)", function(e) {
+    $(this).addClass("checked");
+    $(this).parents(".user").addClass("selected").each(addUser);
+    $("#users").find(".user.selected .checkable:not(.checked)").parents(".user").removeClass("selected").each(removeUser);
     searchGroups();
     e.stopPropagation();
+    e.preventDefault();
+  });
+  $("#people").on("click", ".checkable.checked", function(e) {
+    $(this).removeClass("checked");
+    $(this).parents(".user").removeClass("selected").each(removeUser);
+    searchGroups();
+    e.stopPropagation();
+    e.preventDefault();
+  });
+
+  //
+  $("#people").on("click", ".user", function () {
+    if ($("#users .checkable.checked").size() == 0)
+    {
+      $("#users .user.selected").removeClass("selected").each(removeUser);
+      $(this).addClass("selected").each(addUser);
+      searchGroups();
+    }
   });
 
   // Init UI
